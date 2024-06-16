31778650e492f7a1dc27345cb569a12a6041020b
Ratheronfire <ratheronfire@gmail.com>
2024-06-15 20:49:16 -0400

    Redid castle page with smaller images.

--- hugo-neocities/scripts/resize_images.sh
+++ hugo-neocities/scripts/resize_images.sh
@@ -2,6 +2,6 @@ cd "$1"
 
 for img in *.png; do
 	if [[ $img != "scaled_"* ]]; then
-		ffmpeg -i "$img" -y -vf scale=1280:720 "scaled_${img}" -hide_banner -loglevel error
+		ffmpeg -i "$img" -y -vf scale=640:360 "scaled_${img}" -hide_banner -loglevel error
 	fi
 done
